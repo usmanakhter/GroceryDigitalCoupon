3f7702fefcbc0ac1d573c3fd56edc137d2983e6f
Usman Akhter <usmanakhter1@gmail.com>
2022-01-27 19:09:28 -0600

    GroceryDigitalCoupon

---
+++ JewelCouponClicker.js
@@ -0,0 +1,12 @@
+var jqry = document.createElement('script');
+jqry.src = "https://code.jquery.com/jquery-3.3.1.min.js";
+document.getElementsByTagName('head')[0].appendChild(jqry);
+jQuery.noConflict();
+
+$(function clik() {
+    let clp = document.querySelectorAll(".grid-coupon-btn");
+    var btnToClick = clp;
+    $(btnToClick).each(function () {
+        $(this).click();
+    });
+});
